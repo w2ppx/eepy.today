54fc417d05a1aead51fb18dda008bf76bbb327f1
Damir Modyarov <otomir@yandex.ru>
2024-04-08 14:59:19 +0300

    Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next"
+import type { ReactNode } from "react"
+import { Inter } from "next/font/google"
+import "./globals.css"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+    title: {
+        default: "wappx",
+        template: "%s | wappx",
+    },
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <p className="font-semibold p-4">
+            Edit <code className="rounded px-1 bg-neutral-200">/src/app/page.tsx</code> to get started.
+        </p>
+    )
+}
