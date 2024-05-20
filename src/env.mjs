8e86f654694d839ea8d0875e486afbeb7076dfac
Damir Modyarov <otomir@yandex.ru>
2024-05-20 16:56:55 +0300

    feat: Implement webring widget

--- src/env.mjs
+++ src/env.mjs
@@ -5,6 +5,7 @@ export const env = createEnv({
     server: {
         LASTFM_TOKEN: z.string(),
         LASTFM_USERNAME: z.string(),
+        WEBRING_BASE_URL: z.string().url().optional(),
     },
     client: {},
     experimental__runtimeEnv: {},
