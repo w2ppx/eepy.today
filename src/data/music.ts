551d76b8b43786f217ab053b6ba7517ceedd352f
ppx <very@eepy.today>
2024-07-30 12:40:01 +0300

    Fix page crashing when there are no recent tracks

--- src/data/music.ts
+++ src/data/music.ts
@@ -3,8 +3,15 @@ import { env } from "@/env.mjs"
 
 const client = new SimpleFM(env.LASTFM_TOKEN)
 export async function getNowPlaying() {
-    return await client.user.getRecentTracks({
-        username: env.LASTFM_USERNAME,
-        limit: 5,
-    })
+    try {
+        return await client.user.getRecentTracks({
+            username: env.LASTFM_USERNAME,
+            limit: 5,
+        })
+    } catch (e) {
+        return {
+            tracks: [],
+        }
+    }
+
 }
